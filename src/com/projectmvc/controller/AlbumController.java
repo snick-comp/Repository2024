f6de4aaa7d45066aea9cc33f906a45357d2ee884
snick-comp <snicksonofficial20@gmail.com>
2024-11-04 15:23:04 +0530

    changes made

--- src/com/projectmvc/controller/AlbumController.java
+++ src/com/projectmvc/controller/AlbumController.java
@@ -5,8 +5,12 @@ import com.projectmvc.exception.CustomDatabaseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/albums")
 public class AlbumController {
--- src/main/java/com/projectmvcfinal/ProjectMVCfinalApplication.java
+++ src/main/java/com/projectmvcfinal/ProjectMVCfinalApplication.java
@@ -2,8 +2,7 @@ package com.projectmvcfinal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.projectmvc")
 public class ProjectMVCfinalApplication {
 
     public static void main(String[] args) {
